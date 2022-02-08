88eb17dc5ffb27a4c515712c53633d52f8ed50cc
Matt Kelly <bmatthewkelly@gmail.com>
2022-02-08 15:13:38 -0800

    added ability to submit answers

--- src/components/Answers.js
+++ src/components/Answers.js
@@ -1,21 +1,61 @@
-export default function Answers({ className, disabled = false }) {
+export default function Answers({ className, disabled = false, submitAnswer }) {
+  const handleSubmit = (event, value) => {
+    event.preventDefault();
+    submitAnswer(value);
+  };
+
   return (
     <div
       className={`${className} flex justify-between absolute sm:relative sm:max-w-md sm:mx-auto w-full left-0 px-2`}
     >
-      <button className="answer" disabled={disabled} type="button">
+      <button
+        className="answer"
+        disabled={disabled}
+        onClick={(event) => {
+          handleSubmit(event, 1);
+        }}
+        type="button"
+      >
         Never
       </button>
-      <button className="answer" disabled={disabled} type="button">
+      <button
+        className="answer"
+        disabled={disabled}
+        onClick={(event) => {
+          handleSubmit(event, 2);
+        }}
+        type="button"
+      >
         Meh
       </button>
-      <button className="answer" disabled={disabled} type="button">
+      <button
+        className="answer"
+        disabled={disabled}
+        onClick={(event) => {
+          handleSubmit(event, 3);
+        }}
+        type="button"
+      >
         50/50
       </button>
-      <button className="answer" disabled={disabled} type="button">
+      <button
+        className="answer"
+        disabled={disabled}
+        onClick={(event) => {
+          handleSubmit(event, 4);
+        }}
+        type="button"
+      >
         Sure
       </button>
-      <button className="answer" disabled={disabled} type="button">
+      <button
+        className="answer"
+        disabled={disabled}
+        onClick={(event) => {
+          handleSubmit(event, 5);
+        }}
+        type="button"
+      >
         100%
       </button>
     </div>
--- src/pages/questions.js
+++ src/pages/questions.js
@@ -1,21 +1,16 @@
-import { gql, useQuery } from '@apollo/client';
+import { gql, useLazyQuery, useMutation, useQuery } from '@apollo/client';
 import { shuffle } from 'lodash';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import Answers from '../components/Answers';
 import Page from '../components/Page';
+import { useUser } from '../hooks/User';
 
 const QUESTIONS_QUERY = gql`
-  query QUESTIONS_QUERY {
+  query QUESTIONS_QUERY($id: ID!) {
     questions(
-      where: {
-        answer: {
-          none: {
-            AND: [{ user: { id: { equals: "ckzd4rz8a0028g4pgxjdhmbiy" } } }]
-          }
-        }
-      }
+      where: { answer: { none: { AND: [{ user: { id: { equals: $id } } }] } } }
     ) {
       id
       question
@@ -24,25 +19,87 @@ const QUESTIONS_QUERY = gql`
   }
 `;
 
+const CREATE_ANSWER_MUTATION = gql`
+  mutation CREATE_ANSWER_MUTATION(
+    $userId: ID!
+    $questionId: ID!
+    $answer: Int!
+  ) {
+    createAnswer(
+      data: {
+        answer: $answer
+        user: { connect: { id: $userId } }
+        question: { connect: { id: $questionId } }
+      }
+    ) {
+      id
+    }
+  }
+`;
+
 export default function QuestionsPage() {
   const router = useRouter();
+  const user = useUser();
   const [questions, setQuestions] = useState([]);
   const [questionNumber, setQuestionNumber] = useState(0);
-  const { data, loading, error } = useQuery(QUESTIONS_QUERY, {
-    onCompleted: (res) => {
-      // check to see if there are any questions left
-      if (res.questions && res.questions.length > 0) {
-        setQuestions(shuffle(res.questions));
-      } else {
-        router.push('/results');
-      }
-    },
-  });
+  const [currentPosition, setCurrentPosition] = useState(0);
+
+  const [getQuestions, { data, loading, error }] = useLazyQuery(
+    QUESTIONS_QUERY,
+    {
+      onCompleted: (res) => {
+        // check to see if there are any questions left
+        if (res.questions && res.questions.length > 0) {
+          setQuestions(shuffle(res.questions));
+        } else {
+          router.push('/results');
+        }
+      },
+    }
+  );
+
+  useEffect(() => {
+    if (user?.id) {
+      getQuestions({
+        variables: {
+          id: user.id,
+        },
+      });
+    }
+  }, [user]);
+
+  const [createAnswer, createAnswerMutationResults] = useMutation(
+    CREATE_ANSWER_MUTATION,
+    {
+      onCompleted: () => {
+        const newPosition = currentPosition + 1;
+        if (newPosition < data.questions.length) {
+          setCurrentPosition(newPosition);
+          setQuestionNumber((prevValue) => prevValue + 1);
+        } else {
+          router.push('/results');
+        }
+      },
+    }
+  );
+
   useEffect(() => {
     if (data?.questionsCount) {
       setQuestionNumber(data.questionsCount - data.questions.length + 1);
     }
   }, [data]);
+
+  const submitAnswer = (answer) => {
+    console.log('submitting answer', answer);
+    createAnswer({
+      variables: {
+        userId: user.id,
+        questionId: questions[currentPosition].id,
+        answer,
+      },
+    });
+  };
+
   return (
     <Page>
       <div className="box">
@@ -52,8 +109,8 @@ export default function QuestionsPage() {
             {questionNumber !== 0 && questionNumber}
           </div>
           <h1 className="page-title">It's Easy.</h1>
-          <p>{questions.length && questions[0].question}</p>
-          <Answers className="bottom-4 p-2" />
+          <p>{questions.length && questions[currentPosition].question}</p>
+          <Answers submitAnswer={submitAnswer} className="bottom-4 p-2" />
         </div>
       </div>
     </Page>
